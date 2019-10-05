bbf5cb180c9c491c14eb092b8ff3c6ac999c3852
Keri Rich <keri.n.rich@gmail.com>
2019-10-05 00:51:10 -0600

    pushing quiz content

--- questions.js
+++ questions.js
@@ -1,13 +1,32 @@
 var questions = [
     {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+    title: "When not fighting crime, what is Spider-Man's real name?",
+    choices: ["Peter Benjamin Rogers", "Peter Gabriel Parker, Jr.", "Peter Benjamin Parker", "Paul Simon Peter"],
+    answer: "Peter Benjamin Parker"
     },
     
     {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
-    }
+    title: "What was special about the spider that bit the original Peter Parker?",
+    choices: ["It was radioactive!", "It was genetically engineered!", "It was an alien!"],
+    answer: "It was radioactive!"
+    },
+
+    {
+    title: "How did Peter initially use his powers as Spider-Man?",
+    choices: ["As a novelty television star to try and make money", "As an investment banker on Wall Street", "As a thief and bank robber known as the Man-Spider"],
+    answer: "As a novelty television star to try and make money"
+    },
+
+    {
+    title: "What is his family's famous motto?",
+    choices: ["With great gifts comes great curses", "Go big or go home", "With great power comes great responsibility"],
+    answer: "With great power comes great responsibility"
+    },
+    
+    {
+    title: "Peter's best friend, Harry, is the son of which famous villain?",
+    choices: ["Doctor Octopus", "The Vulture", "Green Goblin"],
+    answer: "Green Goblin"
+    },
+    
 ]
--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var questionTitle = document.getElementById("question-title");
 var buttonAnswers = document.getElementById("button-answers");
 var finalPage = document.getElementById("final-page");
 var timeRemaining = document.getElementById("time-remaining");
+var responseLine = document.getElementById("response");
+var highScore = document.getElementById("view-score");
+var spidermanImage = document.getElementById("spiderman-image");
 
 var questionsIndex = 0;
 var questionsRemaining = questions.length - parseInt(questionsIndex);
@@ -14,8 +17,20 @@ var timerInterval = 0;
 var secondsLeft = questions.length * 15
 var index = "";
 var score = 0;
+var finalList = {
+    initialsList: [],
+    scoresList: [],
+}
 
 // Functions
+function startOverPage() {
+    spidermanImage.appendChild(startPage);
+    startPage.appendChild(questionPage);
+    questionPage.appendChild(finalPage);
+    finalPage.appendChild(responseLine);
+    setTime();
+}
+
 function setTime() {
     var timerInterval = setInterval(function() {
         timeRemaining.textContent = "Time: " + secondsLeft;
@@ -71,36 +86,124 @@ function stopTimer() {
 }  
 
 function renderFinalPage() {
+    var finalPageScore = document.createElement("div");
+    var finalPageHeader = document.createElement("div");
+    var finalPageForm = document.createElement("form");
+    var finalPageLabel = document.createElement("label");
+    var finalPageInput = document.createElement("input");
+    var finalPageSubmit = document.createElement("button");
+    
+    finalPageScore.setAttribute("id","final-score");
+    finalPageHeader.setAttribute("id","final-text");
+    finalPageForm.setAttribute("id","input-form");
+    finalPageLabel.setAttribute("for", "Enter Initials");
+    finalPageInput.setAttribute("id", "input-field");
+    finalPageInput.setAttribute("type", "text");
+    
+    finalPage.appendChild(finalPageScore);
+    finalPage.appendChild(finalPageHeader);
+    finalPage.appendChild(finalPageForm);
+    finalPageForm.appendChild(finalPageLabel);
+    finalPageForm.appendChild(finalPageInput);
+    finalPageForm.appendChild(finalPageSubmit);
+    
+    
+    responseLine.innerHTML = "";
+    finalPageScore.innerHTML = "Score: " + score + " out of 5";
+    finalPageHeader.innerHTML = "Congratulations! You have completed the Spider-Man test! You can save your score below.";
+    finalPageLabel.innerHTML = "Enter Initials";
+    finalPageSubmit.innerHTML = "Submit";
+    
+    function storeScores() {
+        localStorage.setItem("Initials",JSON.stringify(finalList.initialsList));    
+        localStorage.setItem("Score", JSON.stringify(finalList.scoresList));
+    }
+        finalPageSubmit.addEventListener("click", function(event) {
+            event.preventDefault();
+    
+            var initialsText = finalPageInput.value;
+    
+            finalList.initialsList.push(initialsText);
+            finalList.scoresList.push(score);
+            finalPageInput.value = "";
+    
+            storeScores();
+            viewHighScore();
+    })
+}
 
+function viewHighScore() {
+    startPage.innerHTML = "";
+    spidermanImage.innerHTML = "";
+    questionPage.innerHTML = "";
+    finalPage.innerHTML = "";
+
+    var storedInitials = JSON.parse(localStorage.getItem("finalList.initialsList"));
+    var storedScores = JSON.parse(localStorage.getItem("finalList.scoresList"));
+    
+    finalList.initialsList = storedInitials;
+    finalList.scoresList = storedScores;
+    
+    for (var i=0; i<finalList.length; i++) {
+        var initial = finalList.initialsList[i];
+        var highScores = finalList.scoresList[i];
+        var li = document.createElement("li");
+        li.textContent = initial + ": " + highScores;
+        li.setAttribute("scores-list",i);
+        spidermanImage.appendChild(li);
+    }
+    
+    var startOverButton = document.createElement("button");
+    var clearButton = document.createElement("button");
+
+    startOverButton.textContent = "Start Over";
+    clearButton.textContent = "Clear Scores";
+    
+    spidermanImage.appendChild(startOverButton);
+    spidermanImage.appendChild(clearButton);
+    
+    startOverButton.addEventListener("click",startOverPage);
+    clearButton.addEventListener("click", function() {
+        finalList = {
+            initialsList: [],
+            scoresList: [],
+        }
+    })
 }
 
 // Event Listeners
 startButton.addEventListener("click", setTime);
+highScore.addEventListener("click", viewHighScore);
+
+
+
 buttonAnswers.addEventListener("click", function() {
-        var userChoice = event.target;
-        if (userChoice.matches("button")) {
-            console.log(userChoice);
-            userChoice = event.target.id;
-            console.log(userChoice);
-        }
-        
-        if (questions[questionsIndex].choices[userChoice] === questions[questionsIndex].answer) {
-            console.log("yes");
-            console.log(secondsLeft);
-            score = score + 1;
-            buttonAnswers.innerHTML = "";
-            questionsIndex++;
-            questionsRemaining = questionsRemaining - questionsIndex;
-            checkQuestion();
-        }
-        
-        else {
-            console.log("no");
-            console.log(secondsLeft);
-            buttonAnswers.innerHTML = "";
-            secondsLeft = secondsLeft - 15;
-            questionsIndex++;
-            questionsRemaining = questionsRemaining - questionsIndex;
-            checkQuestion();
-        }
+    var userChoice = event.target;
+    if (userChoice.matches("button")) {
+        console.log(userChoice);
+        userChoice = event.target.id;
+        console.log(userChoice);
+    }
+    
+    if (questions[questionsIndex].choices[userChoice] === questions[questionsIndex].answer) {
+        console.log("yes");
+        console.log(secondsLeft);
+        score++;
+        buttonAnswers.innerHTML = "";
+        responseLine.innerHTML = "Correct";
+        questionsIndex++;
+        questionsRemaining = questionsRemaining - questionsIndex;
+        checkQuestion();
+    }
+    
+    else {
+        console.log("no");
+        console.log(secondsLeft);
+        buttonAnswers.innerHTML = "";
+        responseLine.innerHTML = "Wrong";
+        secondsLeft = secondsLeft - 15;
+        questionsIndex++;
+        questionsRemaining--;
+        checkQuestion();
+    }
 })
